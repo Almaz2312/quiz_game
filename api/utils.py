e7ca137a1e8f2601874c0a0e388283a3d9f2be89
Дурусалиев Алмаз Муратович <adurusaliev@megacom.local>
2023-05-23 18:22:23 +0600

    feat: added commentaries in utils to describe logic

--- api/utils.py
+++ api/utils.py
@@ -20,15 +20,24 @@ async def fetch_questions(question_num: int):
 
 
 async def check_save_questions(questions: list, db: Session):
+
+    # counter to count how many questions have been found
     counter = 0
+
+    # Get all questions to a list
     question_ids = [question["id"] for question in questions]
+
+    # Get all existing questions ids
     existing_questions_ids = (
         db.query(Quiz.question_number)
         .filter(Quiz.question_number.in_(question_ids))
         .all()
     )
+
+    # Get ids from tuple to a list
     existing_questions_ids = [qid for qid, in existing_questions_ids]
 
+    # Add unique questions to a list as an object or increase counter
     questions_to_save = []
     for question in questions:
         if question["id"] in existing_questions_ids:
@@ -43,6 +52,7 @@ async def check_save_questions(questions: list, db: Session):
                 )
             )
 
+    # If unique questions list is not empty then bulk save it
     if questions_to_save:
         db.bulk_save_objects(questions_to_save)
         db.commit()
@@ -52,6 +62,8 @@ async def check_save_questions(questions: list, db: Session):
 
 async def process_question_list(db: Session, question_num: int):
     last_save_question = db.query(Quiz).order_by(Quiz.id.desc()).first()
+
+    # loops until it finishes saving unique number of questions
     questions_num_to_post = question_num
     while questions_num_to_post > 0:
         questions = await fetch_questions(question_num)
