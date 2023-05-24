47c92d2722c637d0dea569f8778c60de4523ea13
Дурусалиев Алмаз Муратович <adurusaliev@megacom.local>
2023-05-24 10:43:24 +0600

    fix: deleted unnecessary libraries

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-
-from core.sessions import engine
-from core.config import settings
 
 from api.api_router import api_router
 from core.base_class import Base
+from core.config import settings
+from core.sessions import engine
 
 
 def include_router(app):
@@ -18,10 +16,16 @@ def create_tables():
 
 
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
-    app.add_middleware(CORSMiddleware, allow_origins=["*"],
-                       allow_credentials=True, allow_methods=["*"],
-                       allow_headers=["*"])
+    app = FastAPI(
+        title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     include_router(app)
     create_tables()
     return app
