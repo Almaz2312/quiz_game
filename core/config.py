8f2f88e83bd3a441e5359b9110a1781ecb6946a2
Дурусалиев Алмаз Муратович <adurusaliev@megacom.local>
2023-05-25 16:34:17 +0600

    fix: run pre-commit

--- core/config.py
+++ core/config.py
@@ -1,23 +1,27 @@
 import os
-from dotenv import load_dotenv
-
 from pathlib import Path
 
-env_path = Path('.') / '.env'
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
 
 class Settings:
-    PROJECT_NAME: str = 'Quiz Game'
-    PROJECT_VERSION: str = '1.0.0'
+    PROJECT_NAME: str = "Quiz Game"
+    PROJECT_VERSION: str = "1.0.0"
 
-    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-    POSTGRES_HOST: str = os.getenv('POSTGRES_HOST')
-    POSTGRES_PORT: str = os.getenv('POSTGRES_PORT', 5432)
-    POSTGRES_DB: str = os.getenv('POSTGRES_DB')
-    DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    DATABASE_URL = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
+        f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    )
 
     QUIZ_URL = os.getenv("QUIZ_URL")
+
 
 settings = Settings()
